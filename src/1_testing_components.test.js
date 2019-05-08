190c0352242c0f33e9813bee7eb9d0620858bc84
Jonathan Lorimer <jonathan_lorimer@mac.com>
2019-05-07 22:45:36 -0400

    started

---
+++ src/1_testing_components.test.js
@@ -0,0 +1,11 @@
+import { create } from "react-test-renderer";
+import MyBasicComponent from "./1_testing_components";
+
+describe("MyBasicComponent", () => {
+  it("renders component correctly with react", () => {
+    /*
+      Fill in the test using react test renderer to test how the component renders
+      */
+    expect(false).toBe(true);
+  });
+});
